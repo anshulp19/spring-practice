f005f24123809f67c5e7734a5e76a13231fc787c
Anshul Pandey <13178961+anshulp19@users.noreply.github.com>
2019-03-24 00:12:39 +0530

    Create BaseBallCoach.java

---
+++ src/com/anshul/springdemo/BaseBallCoach.java
@@ -0,0 +1,9 @@
+package com.anshul.springdemo;
+
+public class BaseBallCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice.";
+    }
+}
